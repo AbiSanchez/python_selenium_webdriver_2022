c53f60465143816c735e20427d7ea5f928882f54
Abigail Sanchez <a.s.cruz0291@gmail.com>
2022-04-26 21:02:15 -0500

    ejmeplos de test

---
+++ clase 2_hola mundo/test_example.py
@@ -0,0 +1,14 @@
+class Testsuite:
+
+    def test_first(self):
+        pass
+
+    def test_second(self):
+        pass
+    
+    def test_third(self):
+        pass
+
+    def do_something (self):
+        print('Do something')
+        
