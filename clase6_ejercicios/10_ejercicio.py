787ede89505b65ea8699ccba94ce6e0a0d95faf6
Abigail Sanchez <a.s.cruz0291@gmail.com>
2022-05-03 21:28:30 -0500

    ejercicio del 3may

---
+++ clase6_ejercicios/10_ejercicio.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+# Setup
+chrome_driver_path = 'drivers/chromedriver'
+gecko_driver_path = 'drivers/geckodriver'
+url = 'https://demo.seleniumeasy.com/bootstrap-alert-messages-demo.html'
+service = Service(chrome_driver_path)
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 5)
+
+# Open Web Page
+driver.get(url)
+
+# Boton autoclosable
+locator = (By.ID, 'autoclosable-btn-success')
+start_download: WebElement = wait.until(EC.element_to_be_clickable(locator))
+assert start_download.is_displayed(),'Boton no se muestra'
+start_download.click()
+
+# mensaje
+locator2 =(By.CSS_SELECTOR,'.alert-autocloseable-success')
+assert wait.until(EC.visibility_of_element_located(locator2)), 'No se muestra el mensaje'
+time.sleep(5)
+assert wait.until(EC.invisibility_of_element_located(locator2)), 'Se muestra el mensaje'
+
+
+# close browser
+driver.quit()
