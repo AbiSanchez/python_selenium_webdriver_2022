f73317212bba6630f4dbedaee9f2bf9014e74cb0
Abigail Sanchez <a.s.cruz0291@gmail.com>
2022-05-12 20:57:57 -0500

    modificaciones de ejecrcicio

--- clase4_find_element/01_ejercicio.py
+++ clase4_find_element/01_ejercicio.py
@@ -3,17 +3,25 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from lib.factory.factory_driver import get_driver
 
-# Inicializar driver
-chrome_driver_path = 'drivers/chromedriver'
-gecko_driver_path = 'drivers/geckodriver'
-url = 'https://laboratorio.qaminds.com/'
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service)
-driver.maximize_window()
+
+# # Inicializar driver
+# chrome_driver_path = 'drivers/chromedriver'
+# gecko_driver_path = 'drivers/geckodriver'
+# url = 'https://laboratorio.qaminds.com/'
+# service = Service(chrome_driver_path)
+# driver = webdriver.Chrome(service=service)
+# driver.maximize_window()
+
+# # Abrir pagina
+# driver.get(url)
+
+#Inicializar driver
+driver = get_driver ("chrome")
 
 # Abrir pagina
-driver.get(url)
+driver.get('https://laboratorio.qaminds.com/')
 
 # busqueda en web
 barra : WebElement = driver.find_element(By.XPATH,'//*[@id="search"]/input')
@@ -24,7 +32,7 @@ assert boton_buscar.is_displayed(), 'No se encuentra boton'
 boton_buscar.click()
 time.sleep(5)
 
-iphone : WebElement =driver.find_element(By.XPATH, '//*[@id="content"]/div[3]/div/div/div[1]/a/img')  #TODO - MEJORAR XPATH
+iphone : WebElement =driver.find_element(By.XPATH, '//*[@id="content"]/div[3]/div/div/div[1]/a/img') 
 assert iphone.is_displayed(), 'No se encuentra imagen de iphone'
 
 # Cerrar navegador
